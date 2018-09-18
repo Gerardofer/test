bdca955dc5c1208e9d4a03610234b20b16fe8008
Gerardo Fernandez <gerardojose13@gmail.com>
2018-09-18 00:00:20 -0700

    adds google auth callback

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const epxress = require("express");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oath20").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("./config/keys");
 
 const app = epxress();
+
+const PORT = process.env.PORT || 5000;
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: keys.googleClientID,
-      clientSecrest: keys.googleClientSecret,
+      clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback"
     },
     accessToken => {
@@ -18,10 +20,13 @@ passport.use(
   )
 );
 
-const PORT = process.env.PORT || 5000;
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
-app.get("/", (req, res) => {
-  res.send({ name: "Gerardo Fernandez" });
-});
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 app.listen(PORT);
