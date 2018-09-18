a6575cbcb1c8f563819896b944725d5935ec47c2
Gerardo Fernandez <gerardojose13@gmail.com>
2018-09-17 22:54:57 -0700

    adds google oauth configuration

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const epxress = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oath20").Strategy;
+const keys = require("./config/keys");
+
 const app = epxress();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecrest: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 const PORT = process.env.PORT || 5000;
 
