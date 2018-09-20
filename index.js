ec9cbfa9287538756fbab3c0eda6331004d2d7b1
Gerardo Fernandez <gerardojose13@gmail.com>
2018-09-19 23:39:31 -0700

    refactors server files

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 const epxress = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+require("./services/passport");
 
 const app = epxress();
 
+require("./routes/authRoute")(app);
+
 const PORT = process.env.PORT || 5000;
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("Access Token:", accessToken);
-      console.log("refresh Token:", refreshToken);
-      console.log("profile:", profile);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
 
 app.listen(PORT);
