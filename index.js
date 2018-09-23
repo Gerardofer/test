504b409489c4d33a11ddfd9b561e2073d7a1dba1
Gerardo Fernandez <gerardojose13@gmail.com>
2018-09-23 14:40:18 -0700

    adds cookie-session

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const epxress = require("express");
+const keys = require("./config/keys");
 require("./services/passport");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 
 const app = epxress();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoute")(app);
 
