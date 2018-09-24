bf6ec931a11d7673a0ed1224087ade0d6c970c88
Gerardo Fernandez <gerardojose13@gmail.com>
2018-09-23 18:10:01 -0700

    adds production keys

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+//production keys
+module.exports = {
+  googleClientID:
+    "659944811092-lt23od69g3lo8bp6e9isslliice50cd8.apps.googleusercontent.com",
+  googleClientSecret: "HEvxPd7iMCmZLgtCKQFQbVDa",
+  mySqlHost: "sevafund-prod.c83eturldoby.us-east-1.rds.amazonaws.com",
+  mySqlUser: "gerardofer",
+  mySqlPassword: "S3v4Fund2018",
+  mySqlDatabase: "sevaFund_prod",
+  cookieKey: "sdfgadfgr5wrhfght345665yhdgfhsgfddrythrtghser56245sdfgfgg"
+};
