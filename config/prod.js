fbdfc4ab0de0e909f454b1c31274007257277b71
Gerardo Fernandez <gerardojose13@gmail.com>
2018-09-23 20:28:03 -0700

    finish auth process

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+//production keys
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_LIENT_SECRET,
+  mySqlHost: process.env.MY_SQL_HOST,
+  mySqlUser: process.env.MY_SQL_USER,
+  mySqlPassword: process.env.MY_SQL_PASSWORD,
+  mySqlDatabase: process.env.MY_SQL_DATABASE,
+  cookieKey: process.env.COOKIE_KEY
+};
