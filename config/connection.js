9a7086013bbf417fbee5067f0f8ed1c0215d5fcd
Gerardo Fernandez <gerardojose13@gmail.com>
2018-09-23 22:14:04 -0700

    adds second connection to prod-DB

--- config/connection.js
+++ config/connection.js
@@ -1,43 +1,5 @@
-const Sequelize = require("sequelize");
-const key = require("./keys");
-
-const sequelize = new Sequelize(
-  key.mySqlDatabase,
-  key.mySqlUser,
-  key.mySqlPassword,
-  {
-    host: key.mySqlHost,
-    dialect: "mysql",
-    retry: {
-      match: [
-        /ETIMEDOUT/,
-        /EHOSTUNREACH/,
-        /ECONNRESET/,
-        /ECONNREFUSED/,
-        /ETIMEDOUT/,
-        /ESOCKETTIMEDOUT/,
-        /EHOSTUNREACH/,
-        /EPIPE/,
-        /EAI_AGAIN/,
-        /SequelizeConnectionError/,
-        /SequelizeConnectionRefusedError/,
-        /SequelizeHostNotFoundError/,
-        /SequelizeHostNotReachableError/,
-        /SequelizeInvalidConnectionError/,
-        /SequelizeConnectionTimedOutError/
-      ],
-      max: 5
-    }
-  }
-);
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch(err => {
-    console.error("Unable to connect to the database:", err);
-  });
-
-module.exports = sequelize;
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./connection_prod");
+} else {
+  module.exports = require("./connection_dev");
+}
