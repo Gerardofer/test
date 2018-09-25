bd0d9f5e4ce3b030db54d2b9c9411149611f8427
Gerardo Fernandez <gerardojose13@gmail.com>
2018-09-24 23:18:33 -0700

    sets up JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,28 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./connection_prod");
-} else {
-  module.exports = require("./connection_dev");
-}
+const Sequelize = require("sequelize");
+const key = require("./keys");
+
+const sequelize = new Sequelize(
+  key.mySqlDatabase,
+  key.mySqlUser,
+  key.mySqlPassword,
+  {
+    host: key.mySqlHost,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 1
+    }
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+module.exports = sequelize;
