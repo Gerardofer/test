a53c0e5a9bc92576a5318a58f6ff2a8855fb7ae8
Gerardo Fernandez <gerardojose13@gmail.com>
2018-09-27 23:45:57 -0700

    refactors App.js and auth reducer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 import Header from "./Header";
 import Login from "./Login";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 const Landing = () => <h2>Landing</h2>;
 const Projects = () => <h2>Projects</h2>;
 const ProjectsNew = () => <h2>New Project</h2>;
 const UserProfile = () => <h2>Profile</h2>;
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route path="/login" component={Login} />
-          <Route exact path="/projects" component={Projects} />
-          <Route path="/projects/new" component={ProjectsNew} />
-          <Route path="/user_profile" component={UserProfile} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route path="/login" component={Login} />
+            <Route exact path="/projects" component={Projects} />
+            <Route path="/projects/new" component={ProjectsNew} />
+            <Route path="/user_profile" component={UserProfile} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(App);
