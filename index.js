e8dd726c091f5a9588622d562d18f1b446ac8bbc
Gerardo Fernandez <gerardojose13@gmail.com>
2018-09-29 14:15:48 -0700

    adds production config

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ app.use(passport.session());
 
 require("./routes/authRoute")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(epxress.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
