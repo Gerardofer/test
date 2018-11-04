89e5be0619a0aef09e82b6869c671b8fc45c4485
sandond77 <sandu@ucdavis.edu>
2018-11-04 15:13:24 -0800

    Confirmed that project.js model is successfully creating table in db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,16 @@
 const Profile = require("../models/profile");
+const Project = require("../models/project");
 
 module.exports = app => {
   app.get("/api/personal_info", async (req, res) => {
     const profiles = await Profile.findAll();
     res.send(profiles);
   });
+
+  app.get("api/projects"), async (req, res) => {
+    const projects = await Project.findAll();
+    res.send(projects);
+  }
 
   app.post("/profile", async (req, res) => {
     // const profile = await Profile.create();
